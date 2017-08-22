7fc438afac357f00ee6461d554c39920f524e545
ipang dwi <admin@firstplato.com>
2017-08-22 15:41:27 +0700

    update config.php
    second commit..

--- configdb.php
+++ configdb.php
@@ -2,8 +2,8 @@
 	@session_start();
 	$_SESSION['judul'] = 'SPK KONTRASEPSI';
 	$_SESSION['welcome'] = 'APLIKASI PENDUKUNG KEPUTUSAN PEMILIHAN ALAT KONTRASEPSI';
-	$_SESSION['by'] = 'Adit, S.Kom';
-	$mysqli = new mysqli('localhost','root','1717','spk-adit');
+	$_SESSION['by'] = 'Firstplato.com';
+	$mysqli = new mysqli('localhost','root','','spk-ahp');
 	if($mysqli->connect_errno){
 		echo $mysqli->connect_errno." - ".$mysqli->connect_error;
 		exit();
